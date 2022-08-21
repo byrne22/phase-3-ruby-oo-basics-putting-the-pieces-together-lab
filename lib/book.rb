ab269bd6ee7c565dfc3c8648e02c5518976e3ae9
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-08-21 16:38:46 +0300

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book 
+    attr_accessor :title, :author, :page_count, :genre
 
 
+    def initialize(title)
+        @title=title
+    
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+java = Book.new("And Then There Were None")
+puts java.title
+puts java.author="JJ"
